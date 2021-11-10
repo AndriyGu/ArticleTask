c389685775dd60b535abd5c65da90981ae5f5b42
AndriyGu <andriy.gumich@gmail.com>
2021-11-10 15:32:54 +0300

    add Models

---
+++ src/main/java/com/test/ArticleTestTask/ArticleTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.test.ArticleTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleTestTaskApplication.class, args);
+	}
+
+}
